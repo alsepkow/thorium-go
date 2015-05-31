946183ab94b3436306914c869e228d2325cd058f
Hussain Hassam <hussain.hassam21@gmail.com>
2015-05-31 22:25:38 +0000

    command line arguments and adding values to redis lists

--- launchprogram.go
+++ launchprogram.go
@@ -4,6 +4,12 @@ import "github.com/go-martini/martini"
 import "os/exec"
 import "fmt"
 import "strconv"
+import "redis"
+import "bytes"
+import "flag"
+import "math/rand"
+import "time"
+
 
 func Check(prog string) int {
 	acceptedList := []string {"boltactiongame", "test"}
@@ -27,7 +33,31 @@ func Execute(prog string) *exec.Cmd {
 
 }
 
+func RedisPush(cmdI *exec.Cmd) int {
+	spec := redis.DefaultSpec().Password("go-redis")
+	client, e := redis.NewAsynchClientWithSpec(spec)
+	if e!= nil {
+		fmt.Println("error creating client for: ", e)
+	}
+	defer client.Quit()
+	pidString := strconv.Itoa(cmdI.Process.Pid)
+	var buf bytes.Buffer
+	buf.Write([]byte(pidString))
+	_,e = client.Rpush("server:pids", buf.Bytes())
+	if e != nil {
+		fmt.Println("error writing to list")
+		return 0
+	}
+	return 1
+}
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+	var portArg = flag.Int("p",rand.Intn(65000-10000)+10000, "specifies port, default is random int between 10000-65000")
+	var mapArg = flag.String("m", "default map  value", "description of map")
+	flag.Parse()
+	fmt.Println(strconv.Itoa(*portArg))
+	fmt.Println(*mapArg)
 	processL := make([]*exec.Cmd, 100)
 	m := martini.Classic()
 	m.Get("/launch/:name",  func(params martini.Params) string {
@@ -37,10 +67,12 @@ func main() {
 			for i:=0; i<len(processL); i++ {
 				if processL[i]==nil {
 					processL[i]=cmdInfo
+					//suc := RedisPush(cmdInfo)
+					RedisPush(cmdInfo)
 					break
 				}
 			}
-			fmt.Println(processL)
+			//fmt.Println(processL)
 			return "launching " + params["name"] + "with pid " + strconv.Itoa(cmdInfo.Process.Pid) 	
 		} else {
 			return "not accepted"
